5f5c1d93bb807fde93939454a560e4255564fbe8
Apichet Sittipon <54353591+dewapichet@users.noreply.github.com>
2020-09-18 16:06:03 +0700

    eiei

--- screens/ResumeForm.js
+++ screens/ResumeForm.js
@@ -1,92 +1,112 @@
 import React from 'react'
+import { View, Text, TextInput, StyleSheet, Button, Alert, Platform,ScrollView } from 'react-native'
 import ValidationComponent from 'react-native-form-validator'
-import {View,StyleSheet,Text,TextInput,Button} from 'react-native'
+import axios from 'axios'
+import Camera from '../components/Camera'
 
-export default class ResumeForm extends ValidationComponent{
-    state = {
-        name: '',
-        nickname: '',
-        age: '',
-        skill: ''
-    }
+export default class ResumeForm extends ValidationComponent {
+  state = {
+    name: '',
+    nickname: '',
+    age: '',
+    skill: '',
+    avatar:''
+  }
 
-    _onSubmit = () =>{
-        this.validate({
-            name: {required:true},
-            nickname: {required:true },
-            age: {required:  true, numbers: true},
-            skill: {required:true}
+  _onSubmit = () => {
+    const isValid = this.validate({
+      avatar: {required:true},
+      name: { required: true },
+      nickname: { required: true },
+      age: { required: true, numbers: true },
+      skill: { required: true },
+    });
+    if(isValid){
+      const formData = new FormData();
+      const uri = this.state.avatar
+      formData.append('avatar',{
+          uri:Platform.OS === "android" ? uri : uri.replace('file://',''),
+          type:'image/jpeg',
+          name:'avatar.jpg'
+      })
+      formData.append('name',this.state.name)
+      formData.append('nickname',this.state.nickname)
+      formData.append('age',this.state.age)
+      formData.append('skill',this.state.skill)
+      const config = {
+        headers: { 'content-type': 'multipart/form-data' }
+      }
+      axios.post('https://movie-api.igeargeek.com/users/register', formData, config)
+        .then((res) => {
+          Alert.alert(
+            'Create success',
+            'Click OK go to resume page',
+            [{
+              text: 'OK', onPress: () => {
+                this.props.navigation.push('ResumeDetail', { id: res.data.id })
+              }
+            }]
+          )
+        }).catch((error) => {
+          console.log('error :', error)
         })
     }
+  }
 
-
-
-    render(){
-        return(
-            <View style={styles.container}>
-                <View>
-                    <Text style={styles.getErrorMessages}>
-                        {this.getErrorMessages()}
-                    </Text>
-                </View>
-                <View>
-                    <Text>Full name</Text>
-                    <TextInput 
-                        style={styles.textInput}
-                        onChangeText={(text) => this.setState({ name:text })}  
-                        value={this.state.name}>
-                    </TextInput>
-                </View>
-                <View style={{ marginTop:20}}>
-                    <Text>Nickname</Text>
-                    <TextInput 
-                        style={styles.textInput}
-                        onChangeText={(text) => this.setState({ nickname:text })}  
-                        value={this.state.nickname}>
-                    </TextInput>
-                </View>
-                <View style={{ marginTop:20}}>
-                    <Text>Age</Text>
-                    <TextInput 
-                        style={styles.textInput}
-                        onChangeText={(text) => this.setState({ age:text })}  
-                        value={this.state.age}>
-                    </TextInput>
-                </View>
-                <View style={{ marginTop:20}}>
-                    <Text>skill</Text>
-                    <TextInput 
-                        style={styles.textAreaInput}
-                        onChangeText={(text) => this.setState({ skill:text })}  
-                        value={this.state.skill}
-                        multiline={true}>
-                    </TextInput>
-                </View>
-                <View style={{marginTop:20}}>
-                    <Button title="Create Resume" onPress={this._onSubmit}></Button>
-                </View>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.errorMessage}>
+          {this.getErrorMessages()}
+        </Text>
+        <Camera onTakePicture ={(pictureUri)=> {this.setState({avatar:pictureUri})}} />
+        <View>
+          <Text>Full Name</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={text => this.setState({ name: text })}
+            value={this.state.name}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Text>Nickname</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={text => this.setState({ nickname: text })}
+            value={this.state.nickname}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Text>Age</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={text => this.setState({ age: text })}
+            value={this.state.age}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Text>Skill</Text>
+          <TextInput
+            style={styles.textAreaInput}
+            onChangeText={text => this.setState({ skill: text })}
+            value={this.state.skill}
+            multiline={true}
+          />
+        </View>
+        <View style={{ marginTop: 10,marginBottom:80}}>
+          <Button
+            onPress={this._onSubmit}
+            title="Create Resume"
+          />
+        </View>
+      </ScrollView>
+    )
+  }
 }
+
 const styles = StyleSheet.create({
-    container:{
-        padding: 30, 
-        backgroundColor:'white', 
-        minHeight: '100%'
-    },
-    textInput:{
-        height: 40,
-        borderColor:'gray',
-        borderWidth:1
-    },
-    textAreaInput:{
-        height:100,
-        borderColor:'gray',
-        borderWidth :1
-    },
-    getErrorMessages:{
-        color: 'red',
-        marginBottom:20,
-    }
+  container: { padding: 30, backgroundColor: 'white', minHeight: '100%' },
+  textInput: { height: 40, borderColor: 'gray', borderWidth: 1 },
+  textAreaInput: { height: 100, borderColor: 'gray', borderWidth: 1 },
+  errorMessage: { color: 'red', marginBottom: 20 }
 })
